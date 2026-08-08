9f962ab729a38ed16c4973b88e4bdd3abf319255
wandl-6A72h <hiwepy@gmail.com>
2026-08-08 20:25:09 +0800

    docs: correct @author tag to [@Loong Wan](https://github.com/loong10k)
    - Replace incorrect 'wandl' / 'hiwepy' / 'easy-4-java contributors' attributions
      with the canonical author reference [@Loong Wan](https://github.com/loong10k)
    - Apply to both generated unit tests and existing source files
    - Consistent with project maintenance ownership

--- src/main/java/org/owasp/esapi/spring/boot/csrf/CsrfguardJavascriptServletProperties.java
+++ src/main/java/org/owasp/esapi/spring/boot/csrf/CsrfguardJavascriptServletProperties.java
@@ -17,7 +17,7 @@ package org.owasp.esapi.spring.boot.csrf;
 
 /**
  * TODO
- * @author ： <a href="https://github.com/hiwepy">hiwepy</a>
+ * @author [@Loong Wan](https://github.com/loong10k)
  */
 public class CsrfguardJavascriptServletProperties {
 
